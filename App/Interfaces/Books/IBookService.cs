375cef332308442bc19d00e418e81c1e4eec8ee1
duvian <duvianmartin@gmail.com>
2024-07-29 16:53:45 -0500

    se suben cambios en interfaces y servicios de employers

--- App/Interfaces/Books/IBookService.cs
+++ App/Interfaces/Books/IBookService.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Library.App.Interfaces.Books
 {
     public interface IBookService
     {
-        
+        IEnumerable<Employers>
     }
 }
--- App/Interfaces/Employers/IEmployersService.cs
+++ App/Interfaces/Employers/IEmployersService.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.Models;
 
 namespace Library.App.Interfaces.Employers
 {
     public interface IEmployersService
     {
-        
+        IEnumerable<Employee> GetAllEmployees();
+        Employee GetEmployeeById(int id);
+        void AddEmployee(Employee employee);
+        void UpdateEmployee(int Id, Employee employee);
+        //void DeleteEmployee (int Id);
     }
 }
