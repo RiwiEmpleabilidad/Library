f3c3ab6ec56240e3f63e88772eca1e89f1d54e53
duvian <duvianmartin@gmail.com>
2024-07-29 18:56:57 -0500

    corrijo error en mi tarea

--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 using Library.App.Interfaces.Books;
-using Library.App.Interfaces.Users;
-using Library.App.Services;
-using Library.App.Services.Books;
-=======
 using Library.App.Interfaces.Jwt;
 using Library.App.Interfaces.Users;
 using Library.App.Services;
+using Library.App.Services.Books;
 using Library.App.Services.Jwt;
->>>>>>> 3bb3a63c33c3bb4040a9c9d56f1e5bb0a4eff87a
 using Library.Data;
 using Library.Utils;
 using Microsoft.EntityFrameworkCore;
